8371d2745e704bc46040b2b201399310d56c444a
Navya <vohrana@sheridancollege.ca>
2024-08-12 00:17:58 +0530

    Initial Commit

---
+++ LittleLemonReservationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemonReservationApp
+//
+//  Created by Navya Vohra on 2024-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleLemonReservationApp/LittleLemonReservationAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  LittleLemonReservationAppApp.swift
+//  LittleLemonReservationApp
+//
+//  Created by Navya Vohra on 2024-08-12.
+//
+
+import SwiftUI
+
+//
+@main
+struct LittleLemonReservationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
