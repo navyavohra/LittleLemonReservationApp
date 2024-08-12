e71aed8ed971a47b53a7c66455db9cdcfe6989cb
Navya <vohrana@sheridancollege.ca>
2024-08-13 00:54:50 +0530

    Added Location View Swift UI

---
+++ LittleLemonReservationApp/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  LittleLemonReservationApp
+//
+//  Created by Navya Vohra on 2024-08-13.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
